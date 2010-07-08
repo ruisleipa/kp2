1bb8d9ed9e1b930742e28e45be2496df26e467c1
ruisleipa <mikko.paukkonen@gmail.com>
2010-07-08 17:40:34 +0300

    Added code for directory listing.

---
+++ src/shared/directory.hpp
@@ -0,0 +1,9 @@
+#ifndef __DIRECTORY_HPP
+#define __DIRECTORY_HPP
+
+#include <string>
+#include <vector>
+
+std::vector<std::string> readDirectory(std::string path,std::string ext="");
+
+#endif // __DIRECTORY_HPP
