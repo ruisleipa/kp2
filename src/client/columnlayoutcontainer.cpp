21ac40edd6828de60669a635036ef8db8318c4d3
ruisleipa <mikko.paukkonen@gmail.com>
2010-07-14 14:27:20 +0300

    Added ColumnLayoutContainer class.

---
+++ src/client/columnlayoutcontainer.cpp
@@ -0,0 +1,52 @@
+#include "columnlayoutcontainer.hpp"
+
+#include "ui.hpp"
+
+void ColumnLayoutContainer::onDraw(Graphics& graphics)
+{
+	int visible_widgets=0;
+
+	for(int i=0;i<getWidgetCount();i++)
+	{
+		Widget* widget=getWidget(i);
+		
+		if(!widget)
+			continue;
+			
+		if(widget->getVisible())
+		{
+			visible_widgets++;
+		}
+	}
+	
+	if(visible_widgets == 0)
+	{
+		return;
+	}
+	
+	Vector2D widgetsize;
+	widgetsize.setX(getSize().getX()/visible_widgets-PADDING*(visible_widgets+1)/visible_widgets);
+	widgetsize.setY(getSize().getY()-PADDING*2);
+	
+	int visible_index=0;
+	
+	for(int i=0;i<getWidgetCount();i++)
+	{
+		Widget* widget=getWidget(i);
+		
+		if(!widget)
+			continue;
+	
+		if(widget->getVisible())
+		{
+			Vector2D pos;
+			pos.setY(PADDING);
+			pos.setX(PADDING*(visible_index+1)+widgetsize.getX()*visible_index);	
+			
+			widget->setPosition(pos);
+			widget->setSize(widgetsize);
+			
+			visible_index++;
+		}
+	}	
+}
---
+++ src/client/columnlayoutcontainer.hpp
@@ -0,0 +1,16 @@
+#ifndef COLUMNLAYOUTCONTAINER_HPP
+#define COLUMNLAYOUTCONTAINER_HPP
+
+#include "container.hpp"
+
+class ColumnLayoutContainer : public Container
+{
+	public:
+		virtual void onDraw(Graphics& graphics);
+	
+	private:
+
+};
+
+#endif // COLUMNLAYOUTCONTAINER_HPP
+
