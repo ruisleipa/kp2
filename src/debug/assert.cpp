85e5e860b8124c1a2832c643b39a8a78a180a73c
ruisleipa <mikko.paukkonen@gmail.com>
2010-08-09 15:37:23 +0300

    Moved the correct assert to debug from client.

--- src/debug/assert.cpp
+++ src/debug/assert.cpp
@@ -2,10 +2,9 @@
 
 #include <iostream>
 
-void _assert(const char* e,const char* file,int line)
+void my_assert(const char* e,const char* file,int line)
 {
 	std::cerr<<"assertion failed: ";
 	std::cerr<<file<<":"<<line<<": ";
-	std::cerr<<e<<std::endl;
-	
+	std::cerr<<e<<std::endl;	
 }
--- src/debug/assert.hpp
+++ src/debug/assert.hpp
@@ -1,10 +1,10 @@
 #ifndef ASSERT_HPP
 #define ASSERT_HPP
 
-void _assert(const char* e,const char* file,int line);
+void my_assert(const char* e,const char* file,int line);
 
 #ifndef NDEBUG
-#define assert(e) ((e) ? (void)0 : _assert(#e, __FILE__, __LINE__))
+#define assert(e) ((e) ? (void)0 : my_assert(#e, __FILE__, __LINE__))
 #else
 #define assert(x) ((void)0)
 #endif
