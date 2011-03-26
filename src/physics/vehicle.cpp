9c97b9b648ed1034329328a65bccb6363618f6ed
ruisleipa <mikko.paukkonen@gmail.com>
2011-03-26 15:42:31 +0200

    Simulation and other development in Vehicle.

--- src/physics/vehicle.cpp
+++ src/physics/vehicle.cpp
@@ -1,67 +1,33 @@
 #include "vehicle.hpp"
 #include "math_tools.hpp"
-#include "shared/inifile.hpp"
-#include "timer.hpp"
 
-#ifdef _THREAD_DEBUG_
-#define TTICKS_COUNT 10000
-#endif
+const int TICKS_PER_SECOND = 300;
 
-#define ENGINE_ITERATIONS 300
 const double g = 9.81;	//m/s
 
-Vehicle::State::State()
+Vehicle::State::State():
+	vehicle_pos(0.0, 0.0, 0.0),
+	m_vehicle_angle(0.0),
+	m_flywheel(0.0),
+	gearbox_out(0.0),
+	m_planet_gear(0.0),
+	m_f_l_tire(0.0),
+	m_f_r_tire(0.0)
 {
+	
 }
 
-Vehicle::State::State(double initval):vehicle_pos(initval, initval, initval)
-{
-	m_vehicle_angle = 0;
-	m_flywheel = initval;
-	m_gearbox_out = initval;
-	m_planet_gear = initval;
-	m_f_l_tire = initval;
-	m_f_r_tire = initval;
-}
-
-Vehicle::State Vehicle::State::operator*(double factor)
-{
-	Vehicle::State tmp;
-	tmp.vehicle_pos = vehicle_pos * factor;
-	tmp.m_vehicle_angle = m_vehicle_angle * factor;
-	tmp.m_flywheel = m_flywheel * factor;
-	tmp.m_gearbox_out = m_gearbox_out * factor;
-	tmp.m_planet_gear = m_planet_gear * factor;
-	tmp.m_f_l_tire = m_f_l_tire * factor;
-	tmp.m_f_r_tire = m_f_r_tire * factor;
-	return tmp;
-}
-
-Vehicle::State& Vehicle::State::operator+=(State& inc)
-{
-	vehicle_pos += inc.vehicle_pos;
-	m_vehicle_angle += inc.m_vehicle_angle;
-	m_flywheel += inc.m_flywheel;
-	m_gearbox_out += inc.m_gearbox_out;
-	m_planet_gear += inc.m_planet_gear;
-	m_f_l_tire += inc.m_f_l_tire;
-	m_f_r_tire += inc.m_f_r_tire;
-	return *this;
-}
 void Vehicle::State::add(double factor, State& dx)
 {
 	vehicle_pos += dx.vehicle_pos * factor;
 	m_vehicle_angle += dx.m_vehicle_angle * factor;
 	m_flywheel += dx.m_flywheel * factor;
-	m_gearbox_out += dx.m_gearbox_out * factor;
+	gearbox_out += dx.gearbox_out * factor;
 	m_planet_gear += dx.m_planet_gear * factor;
 	m_f_l_tire += dx.m_f_l_tire * factor;
 	m_f_r_tire += dx.m_f_r_tire * factor;
 }
 
-	m_current_x(0.0),
-	m_current_v(0.0),
-	m_current_a(0.0)
 Vehicle::Vehicle(Engine& engine, Gearbox& gearbox, Clutch& clutch,
 	Chassis& chassis, Tire& frontLeftTire, Tire& frontRightTire,
 	Tire& backLeftTire, Tire& backRightTire, Brake& frontLeftBrake,
@@ -79,196 +45,46 @@ Vehicle::Vehicle(Engine& engine, Gearbox& gearbox, Clutch& clutch,
 	frontRightBrake(frontRightBrake),
 	backLeftBrake(backLeftBrake),
 	backRightBrake(backRightBrake),
+	m_current_x(),
+	m_current_v(),
+	m_current_a()
 {
 
-	m_log_telemetry = false;
-	m_simulate = false;
-	
 	m_brake_usage=0;
-	m_throttle=0;
-	m_clutch_usage=1;
-	m_ignition=false;
-	m_current_gear=m_gearbox.getNeutralGear();
+	clutch_usage=1;
+	m_current_gear=gearbox.getNeutralGear();
 	
 	return;
 }
 
-Vehicle::~Vehicle()
+void Vehicle::advanceSimulation()
 {
-	toggleTelemetry(false);
-}
-
-int Vehicle::load(const std::string& filename)
-{
-	IniFile in;
-	if (in.load(filename))
-	{
-		std::cerr << "Unable to open \"" << filename << "\"\n";
-		return -1;
-	}
+	double time_chunk = 1.0 / double(TICKS_PER_SECOND);
 	
-	if (m_log_telemetry)
-	{
-		toggleTelemetry(true);
-	}
-
-	return 0;
-}
-
-int Vehicle::toggleTelemetry(bool on)
-{
-	if (on == true && !m_tel_out.is_open())
-	{
-		m_tel_out.open("telemetry",
-				std::ios::out|std::ios::app|std::ios::binary);
-		if (!m_tel_out.is_open())
-		{
-			std::cerr << "couldn't open telemetryfile" << std::endl;
-			return -1;
-		}
-		m_log_telemetry = true;	
-	}
-	else if (on == false && m_tel_out.is_open())
-	{
-		m_tel_out.close();
-	}
-
-	return 0;
-}
-
-int physics(void* data)
-{
-	#ifdef DEBUG_MODE
-	long int loops = 0;
-	#endif
-	/* this function reads these members:
-		inputs
-		timer
-	   this function writes to these members:
-		Current_p
-		m_current_v
-		m_current_a
-	*/
-	Vehicle& dstvehicle = *(reinterpret_cast<Vehicle*>(data));
-	
-	Vehicle::State m_current_x = dstvehicle.m_current_x;	//positions
-	Vehicle::State m_current_v = dstvehicle.m_current_v;	//speeds
-	Vehicle::State m_current_a = dstvehicle.m_current_a;	//accelerations
-
-	#ifdef _THREAD_DEBUG_
-	double time_ticks[TTICKS_COUNT];
-	long int frame = 0;
-	#endif
-
-	timer tmr;
-	double time_chunk;
-	double real_lag = 0.0;
-	double last_time;
-	double start_time = last_time = tmr.get_seconds();
-	while (dstvehicle.m_simulate)
-	{		
-		time_chunk = real_lag / ENGINE_ITERATIONS;
-		
-		for (int i = 0; i < ENGINE_ITERATIONS; ++i)
-		{
-			dstvehicle.getDerivates(&m_current_x, &m_current_v,
-								&m_current_a);
-			m_current_v.add(time_chunk, m_current_a);
-
-			if(fabs(m_current_v.vehicle_pos.y) > 0.09)
-				m_current_x.add(time_chunk, m_current_v);
-		}
-	
-		dstvehicle.m_current_x = m_current_x;
-		dstvehicle.m_current_v = m_current_v;
-		dstvehicle.m_current_a = m_current_a;
-		
-		real_lag = tmr.get_seconds() - last_time;
-		last_time += real_lag;
-		
-		#ifdef _THREAD_DEBUG_
-		if (frame < TTICKS_COUNT)
-		{
-			time_ticks[frame] = real_lag;
-			++frame;
-		}
-		#endif
-		
-		#ifdef DEBUG_MODE
-		++loops;
-		#endif
-	}
-
-	#ifdef DEBUG_MODE
-	std::cout << "Thread finished, average execution rate was " <<
-	0.001 * double (loops * ENGINE_ITERATIONS) /
-	(last_time - start_time) << "kHz\n";
-	#endif
-	
-	#ifdef _THREAD_DEBUG_
-	std::ofstream f("ticks.txt");
-	for (int i = 0; i < frame; ++i)
-		f << time_ticks[i] << '\xA';
-	#endif
-	
-	return 0;
-}
-
-int Vehicle::startThread()
-{
-	if (m_simulate)
-		return -1;
-	
-	m_simulate = true;
-	m_enginethread=Thread(physics);
-	m_enginethread.run(this);
-
-	#ifdef DEBUG_MODE
-	std::cout << "Thread #" << m_enginethread.getId() << " started\n";
-	#endif
-
-	return 0;
-}
-
-int Vehicle::stopThread()
-{
-	int retval;
-	m_simulate = false;
-
-	#ifdef DEBUG_MODE
-	std::cout << "Waiting thread #" << m_enginethread.getId() <<
-	" to finish\n";
-	#endif
-
-	retval = m_enginethread.wait();
-	return retval;
+	getDerivates(&m_current_x, &m_current_v, &m_current_a);
+	m_current_v.add(time_chunk, m_current_a);
+	m_current_x.add(time_chunk, m_current_v);
 }
 
 void Vehicle::getDerivates(State* p0, State* v0, State* a0)
 {
+	double velocity = v0->position;
+	double acceleration = a0->position;	
+	
 	double weight_rear = g * chassis.getRearMass() + chassis.getCenterOfGravityHeight() /
 		chassis.getWheelbase() * chassis.getTotalMass() *
 								acceleration;
 	double weight_front = chassis.getTotalMass() * g - weight_rear;
-	// Use previous first and second order State derivates
-	// to solve current second order State derivates
-	tVector<double> vehicle_alignment(0,1, 0.0);
-	// vehicle's longitudinal speed
-	double vehicle_speed = v0->vehicle_pos.DotProduct(vehicle_alignment);
-	// vehicle's longitudinal acceleration
-	double vehicle_acc = a0->vehicle_pos.DotProduct(vehicle_alignment);
-
-	double engine_overall_torque = 0.0;
-	double gearbox_overall_torque = 0.0;
-
 
 	double r_l_tire = v0->gearbox_out /	gearbox.getDifferentialRatio() - v0->m_planet_gear;
 	double r_r_tire = r_l_tire + 2.0 * v0->m_planet_gear;
 
 	// absolute value of the longitudinal speed
-	double _abs_veh_vel_ = dabs(vehicle_speed);
+	double _abs_veh_vel_ = dabs(velocity);
 
-	// overall_ratio = m_gearbox.differential_ratio * m_gearbox.ratio();
+	double engine_overall_torque = 0.0;
+	double gearbox_overall_torque = 0.0;	
+		
 	// Consider how to get the gear ratio
 	if (m_current_gear!=gearbox.getNeutralGear())
 	{
@@ -278,7 +94,7 @@ void Vehicle::getDerivates(State* p0, State* v0, State* a0)
 		engine_overall_torque += clutch_torque_deliver;
 	}
 
-	engine_overall_torque += m_engine.getTorque(m_throttle,m_ignition,v0->m_flywheel);
+	engine_overall_torque += m_engine.getTorque(v0->m_flywheel);
 	
 	// determine the slip ratio, notice, that this won't get your car
 	// moving if your pacjeka curve reaches zero when sr != 0
@@ -335,8 +151,11 @@ void Vehicle::getDerivates(State* p0, State* v0, State* a0)
 	// this model needs to do with gearbox loss of energy!
 	gearbox_overall_torque += (rear_left_trq + rear_right_trq) / gearbox.getDifferentialRatio();
 
+	
+	
+	
 	a0->m_flywheel = engine_overall_torque / m_engine.getFlywheelInertia();
-	a0->m_gearbox_out = gearbox_overall_torque /
+	a0->gearbox_out = gearbox_overall_torque /
 		(
 		 	gearbox.getDriveshaftInertia() +
 		 	(
@@ -380,12 +199,12 @@ float Vehicle::getThrottle()
 
 void Vehicle::setClutchUsage(float usage)
 {
-	m_clutch_usage=usage;
+	clutch_usage=usage;
 }
 
 float Vehicle::getClutchUsage()
 {
-	return m_clutch_usage;
+	return clutch_usage;
 }
 
 void Vehicle::setIgnition(bool ignition)
@@ -393,7 +212,7 @@ void Vehicle::setIgnition(bool ignition)
 	m_ignition=ignition;
 }
 
-float Vehicle::getIgnition()
+bool Vehicle::getIgnition()
 {
 	return m_ignition;
 }
@@ -403,7 +222,7 @@ void Vehicle::setGear(int gear)
 	m_current_gear=gear;
 }
 
-float Vehicle::getGear()
+int Vehicle::getGear()
 {
 	return m_current_gear;
 }
@@ -413,6 +232,15 @@ float Vehicle::getEngineSpeed()
 	return m_current_v.m_flywheel * RADS_TO_RPM;
 }
 
+int Vehicle::getCurrentStep()
+{
+	return step;
+}
 
-
-
+int Vehicle::getLagInSteps(double realTime)
+{
+	double stepTime = 1.0 / double(TICKS_PER_SECOND);
+	double simulationTime = getCurrentStep() * stepTime;
+	
+	return int((realTime - simulationTime) / stepTime);
+}
--- src/physics/vehicle.hpp
+++ src/physics/vehicle.hpp
@@ -2,19 +2,15 @@
 #define _VEHICLE_HPP
 
 #include <string>
-#include <vector>
-#include <iostream>
-#include <fstream>
-#include <cmath>
-#include "math_tools.hpp"
-#include "shared/thread.hpp"
 
-#include "physics/engine.hpp"
-#include "physics/gearbox.hpp"
-#include "physics/tire.hpp"
-#include "physics/clutch.hpp"
-#include "physics/brake.hpp"
-#include "physics/chassis.hpp"
+#include "math_tools.hpp"
+
+#include "engine.hpp"
+#include "gearbox.hpp"
+#include "tire.hpp"
+#include "clutch.hpp"
+#include "brake.hpp"
+#include "chassis.hpp"
 
 class Vehicle
 {
@@ -27,57 +23,38 @@ class Vehicle
 		
 		class State
 		{
-		public:
-			tVector<double> vehicle_pos;
-			double m_vehicle_angle;	//radians
-			double m_flywheel;	//rad/s
-			double m_gearbox_out;
-			double m_planet_gear;
-			double m_f_l_tire;
-			double m_f_r_tire;
+			public:
+				double position;
+				double m_flywheel;	//rad/s
+				double m_gearbox_out;
+				double m_planet_gear;
+				double m_f_l_tire;
+				double m_f_r_tire;
 
-			State();
-			State(double initval);
-			State operator*(double factor);
-			State& operator+=(State & inc);
-			void add(double factor, State& dx);
+				State();
+				void add(double factor, State& dx);
 		};	
-		enum corner {FRONT_LEFT, FRONT_RIGHT, REAR_LEFT, REAR_RIGHT};
-
-		int load(const std::string& filename);
-
-		friend int physics(void*);
 		
-		int startThread();
-		int stopThread();
-
-		int toggleTelemetry(bool on);
-
-		tVector<double>& getPos(){return m_current_x.vehicle_pos;}
-		void setPos(tVector<double>& pos){m_current_x.vehicle_pos = pos;}
+		tVector<double>& getPos(){return m_current_x.position;}
+		void setPos(tVector<double>& pos){m_current_x.position = pos;}
 	
 		State& getVel(){return m_current_v;}
-		void setVel(State& vel){m_current_v = vel;}
-		
 		State& getAcc(){return m_current_a;}
-		void setAcc(State& acc){m_current_a = acc;}
 		
 		void setBrakeUsage(float usage);
 		float getBrakeUsage();
 		
-		void setThrottle(float throttle);
-		float getThrottle();
-		
 		void setClutchUsage(float usage);
 		float getClutchUsage();
 		
-		void setIgnition(bool ignition);
-		float getIgnition();
-		
 		void setGear(int gear);
-		float getGear();
+		int getGear();
 		
 		float getEngineSpeed();
+		
+		void advanceSimulation();
+		int getCurrentStep();
+		int getLagInSteps(double realTime);
 
 	private:	
 		Engine& engine;
@@ -95,8 +72,6 @@ class Vehicle
 
 		State m_current_x, m_current_v, m_current_a;
 		void getDerivates(State*, State*, State*);
-		Thread m_enginethread;
-		bool m_simulate;
 		
 		float m_brake_usage;
 		float m_throttle;
@@ -104,8 +79,7 @@ class Vehicle
 		bool m_ignition;
 		int m_current_gear;
 		
-		bool m_log_telemetry;
-		std::ofstream m_tel_out;
+		int step;
 };
 
 #endif
