13ca091b9e2f08f64fe7fbff23d996582b6edcf3
ruisleipa <mikko.paukkonen@gmail.com>
2011-05-22 00:38:43 +0300

    Added fitting checks to IntakeManifold.

--- src/server/intakemanifold.cpp
+++ src/server/intakemanifold.cpp
@@ -2,6 +2,7 @@
 
 #include "utils/inifile.hpp"
 #include "vehicle.hpp"
+#include "cylinderhead.hpp"
 
 const std::string& IntakeManifold::getName() const
 {
@@ -13,8 +14,48 @@ int IntakeManifold::getPrice() const
 	return 0;
 }
 
+void IntakeManifold::checkPrerequisiteParts(const Vehicle& vehicle) const
+{
+	for(size_t i = 0; i < vehicle.getPartCount(); ++i)
+	{
+		const Part& part = vehicle.getPart(i);
+		
+		if(part.getType() == "cylinderhead")
+		{
+			const CylinderHead& cylinderHead = part.getModel<CylinderHead>();
+							
+			if(cylinderHead.getCylinderCount() != cylinders)
+				throw PartDoesNotFitException("INTAKEMANIFOLD_CYLINDERCOUNT_DOES_NOT_MATCH");
+			
+			if(cylinderHead.getCylinderAlignment() != cylinderAlignment)
+				throw PartDoesNotFitException("INTAKEMANIFOLD_CYLINDERALIGNMENT_DOES_NOT_MATCH");
+		}		
+	}
+}
+
+void IntakeManifold::checkForExtraPartsOfThisType(const Vehicle& vehicle) const
+{
+	for(size_t i = 0; i < vehicle.getPartCount(); ++i)
+	{
+		if(vehicle.getPart(i).getType() == "intakemanifold")
+			throw PartDoesNotFitException("NO_ROOM_FOR_EXTRA_INTAKEMANIFOLD");
+	}
+}
+
 IntakeManifold::IntakeManifold(IniFile& iniFile):
 	PartModel(iniFile)
 {
-	iniFile.getValue("name",name);
+	iniFile.getValue("cylinders", cylinders);
+	iniFile.getValue("cylinderAlignment", cylinderAlignment);
+		
+	std::stringstream ss;
+	
+	ss << cylinderAlignment;
+	ss << cylinders;
+	ss << " ";
+	
+	ss << " -imusarja ";
+	ss << flow;
+	
+	name = ss.str();
 }
--- src/server/intakemanifold.hpp
+++ src/server/intakemanifold.hpp
@@ -14,10 +14,16 @@ class IntakeManifold: public PartModel
 		const std::string& getName() const;
 		int getPrice() const;
 		
+		void checkPrerequisiteParts(const Vehicle& vehicle) const;
+		void checkForExtraPartsOfThisType(const Vehicle& vehicle) const;
+		
 		IntakeManifold(IniFile& iniFile);
 		
 	private:
 		std::string name;
+		std::string camshaftPosition;
+		std::string cylinderAlignment;
+		int cylinders;
 		
 };
 
