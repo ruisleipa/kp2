2627af5bb57f6d8981a11427b0fcc26f2c88223c
ruisleipa <mikko.paukkonen@gmail.com>
2011-05-26 22:52:21 +0300

    Made background loading more flexible.

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -10,6 +10,7 @@
 #include "graphics/texture.hpp"
 #include "graphics/texturecollection.hpp"
 #include "utils/timer.hpp"
+#include "utils/directory.hpp"
 #include "events/events.hpp"
 
 #include "gui/rootcontainer.hpp"
@@ -46,38 +47,26 @@ void startGame()
 	MusicPlayer musicPlayer;
 	
 	LoadingScreen loadingScreen(window);
-	loadingScreen.setTotalLoadCount(13);
-
+	
 	FontLoader fontLoader(window);	
 	
 	TextureCollection backgroundTextures;
 	
-	backgroundTextures.addTexture("image00",Texture("data/images/backgrounds/block.png"));
-	loadingScreen.progress();
-	backgroundTextures.addTexture("image01",Texture("data/images/backgrounds/brakedisk.png"));
-	loadingScreen.progress();
-	backgroundTextures.addTexture("image02",Texture("data/images/backgrounds/charger.png"));
-	loadingScreen.progress();
-	backgroundTextures.addTexture("image03",Texture("data/images/backgrounds/cooler.png"));
-	loadingScreen.progress();
-	backgroundTextures.addTexture("image04",Texture("data/images/backgrounds/cylinderhead.png"));
-	loadingScreen.progress();
-	backgroundTextures.addTexture("image05",Texture("data/images/backgrounds/diff.png"));
-	loadingScreen.progress();
-	backgroundTextures.addTexture("image06",Texture("data/images/backgrounds/engine.png"));
-	loadingScreen.progress();
-	backgroundTextures.addTexture("image07",Texture("data/images/backgrounds/engine1.png"));
-	loadingScreen.progress();
-	backgroundTextures.addTexture("image08",Texture("data/images/backgrounds/engine2.png"));
-	loadingScreen.progress();
-	backgroundTextures.addTexture("image09",Texture("data/images/backgrounds/exhaust.png"));
-	loadingScreen.progress();
-	backgroundTextures.addTexture("image10",Texture("data/images/backgrounds/intake.png"));
-	loadingScreen.progress();
-	backgroundTextures.addTexture("image11",Texture("data/images/backgrounds/tire.png"));
-	loadingScreen.progress();
-	backgroundTextures.addTexture("image12",Texture("data/images/backgrounds/tools.png"));
-	loadingScreen.progress();
+	const std::string BACKGROUND_DIR = "data/images/backgrounds/";
+	
+	std::vector<std::string> backgroundFiles = readDirectory("data/images/backgrounds/");
+	
+	loadingScreen.setTotalLoadCount(backgroundFiles.size());
+	
+	for(std::vector<std::string>::size_type i = 0; i < backgroundFiles.size(); ++i)
+	{
+		std::stringstream ss;
+		
+		ss << "image" << i;
+	
+		backgroundTextures.addTexture(ss.str(), Texture(BACKGROUND_DIR + backgroundFiles[i]));
+		loadingScreen.progress();
+	}	
 	
 	TextureCollection mainmenuTextures;
 	mainmenuTextures.addTexture("title",Texture("data/images/kp2_txt.png"));
