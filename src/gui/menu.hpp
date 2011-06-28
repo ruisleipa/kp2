adbe58349f92e2ab82aac2c9291a3f900071d5f8
ruisleipa <mikko.paukkonen@gmail.com>
2011-06-29 00:27:07 +0300

    Menu now contains facilities for easier loading of widgets.

--- src/gui/menu.hpp
+++ src/gui/menu.hpp
@@ -1,14 +1,25 @@
 #ifndef MENU_HPP
 #define MENU_HPP
 
+#include <string>
+
 #include "freecontainer.hpp"
+#include "gui/widgetloader.hpp"
 
 class Menu : public FreeContainer
 {
 	public:
+		void handleEvent(Event* event);
+	
+		Menu(std::string uifile);
 		Menu();
 	
+	protected:
+		virtual void connectFunctionality();
+	
 	private:
+		std::string uifile;
+		std::tr1::shared_ptr<WidgetLoader> loader;
 };
 
 #endif // MENU_HPP
