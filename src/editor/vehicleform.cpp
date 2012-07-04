70ce5842be11649e5c71b0a7d7f88cc75e4d6146
Mikko Paukkonen <mikko.paukkonen@gmail.com>
2012-07-04 20:34:41 +0300

    Added part editing to VehicleForm.

--- src/editor/vehicleform.cpp
+++ src/editor/vehicleform.cpp
@@ -1,4 +1,6 @@
 #include "vehicleform.hpp"
+#include "typespecificformfactory.hpp"
+#include "objectfactory.hpp"
 
 void VehicleForm::updateOriginal()
 {
@@ -11,13 +13,121 @@ void VehicleForm::updateOriginal()
 	vehicle->applyPropertiesOf(Game::Vehicle(name, price, year, info, imageName));
 }
 
+void VehicleForm::setItemIcon(QTreeWidgetItem* item)
+{
+	Game::PartContainer::Slot* slot = itemSlots[item];
+
+	Game::Part* part = slot->getPart();
+
+	if(part)
+		item->setIcon(0, QIcon(QPixmap("data/editor/icons/item.png")));
+	else
+		item->setIcon(0, QIcon(QPixmap("data/editor/icons/empty.png")));
+}
+
+void VehicleForm::addSlotItem(Game::PartContainer::Slot* slot, std::string slotName, QTreeWidgetItem* parent)
+{
+	QStringList texts(QString(slotName.c_str()));
+
+	QTreeWidgetItem* item = new QTreeWidgetItem(parent, texts);
+
+	itemSlots[item] = slot;
+
+	setItemIcon(item);
+
+	if(!parent)
+		partTree->addTopLevelItem(item);
+
+	if(slot->getPart())
+		addSlotsFor(slot->getPart(), item);
+}
+
+void VehicleForm::addSlotsFor(Game::PartContainer* container, QTreeWidgetItem* item)
+{
+	for(auto pair : container->getSlots())
+	{
+		std::string slotName = pair.first;
+		Game::PartContainer::Slot* slot = pair.second;
+
+		addSlotItem(slot, slotName, item);
+	}
+}
+
 VehicleForm::VehicleForm(Game::Vehicle* vehicle, QWidget *parent) :
 	TypeSpecificForm(parent),
 	vehicle(vehicle)
 {
 	setupUi(this);
-	
+
 	nameField->setText(vehicle->getName().c_str());
 	yearField->setValue(vehicle->getYear());
 	infoField->setPlainText(vehicle->getInfo().c_str());
+
+	addSlotsFor(vehicle);
+}
+
+void VehicleForm::openEditor(Game::Object* object)
+{
+	editorContainer->removeWidget(editor.get());
+
+	editor.reset(TypeSpecificFormFactory().createFormFor(object));
+
+	editorContainer->addWidget(editor.get());
+	editor->show();
+
+	connect(editor.get(), SIGNAL(changed()), this, SLOT(objectEdited()));
+}
+
+void VehicleForm::on_partTree_itemClicked(QTreeWidgetItem* selectedItem, int column)
+{
+	(void)column;
+
+	Game::PartContainer::Slot* slot = itemSlots[selectedItem];
+
+	if(slot->getPart())
+	{
+		openEditor(slot->getPart());
+	}
+}
+
+void VehicleForm::on_partTree_itemDoubleClicked(QTreeWidgetItem* selectedItem, int column)
+{
+	(void)column;
+
+	Game::PartContainer::Slot* slot = itemSlots[selectedItem];
+
+	if(!slot->getPart())
+	{
+		for(std::string className : readDirectory("data/editor/templates/"))
+		{
+			Game::Object* object = Editor::ObjectFactory().createObject(className);
+
+			if(Game::Part* part = dynamic_cast<Game::Part*>(object))
+			{
+				try
+				{
+					slot->attachPart(part);
+
+					setItemIcon(selectedItem);
+
+					addSlotsFor(slot->getPart(), selectedItem);
+
+					openEditor(slot->getPart());
+
+					break;
+				}
+				catch(Game::PartContainer::Slot::IncompatibleTypeException)
+				{
+
+				}
+			}
+
+			delete object;
+		}
+	}
+}
+
+void VehicleForm::objectEdited()
+{
+	editor->updateOriginal();
 }
--- src/editor/vehicleform.hpp
+++ src/editor/vehicleform.hpp
@@ -6,6 +6,7 @@
 #include "typespecificform.hpp"
 #include "ui_vehicleform.h"
 #include "game/vehicle.hpp"
+#include <QTreeWidgetItem>
 
 class VehicleForm : public TypeSpecificForm, private Ui::VehicleForm
 {
@@ -13,12 +14,25 @@ class VehicleForm : public TypeSpecificForm, private Ui::VehicleForm
 
 	public:
 		virtual void updateOriginal();
-	
-		explicit VehicleForm(Game::Vehicle* vehicle, QWidget *parent = 0);
+
+		explicit VehicleForm(Game::Vehicle* vehicle, QWidget* parent = 0);
+
 
 	private:
+		void openEditor(Game::Object *object);
+		void setItemIcon(QTreeWidgetItem* item);
+		void addSlotsFor(Game::PartContainer *container, QTreeWidgetItem *item = 0);
+		void addSlotItem(Game::PartContainer::Slot* slot, std::string slotName, QTreeWidgetItem* parent);
+
+		std::map<QTreeWidgetItem*, Game::PartContainer::Slot*> itemSlots;
 		Game::Vehicle* vehicle;
-	
+		std::unique_ptr<TypeSpecificForm> editor;
+
+	private slots:
+		void on_partTree_itemClicked(QTreeWidgetItem *selectedItem, int column);
+		void on_partTree_itemDoubleClicked(QTreeWidgetItem *selectedItem, int column);
+		void objectEdited();
+
 };
 
 #endif
