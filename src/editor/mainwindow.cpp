e15b712215b491356ab18ef3f928d043d33ccb2a
Mikko Paukkonen <mikko.paukkonen@gmail.com>
2012-07-04 20:29:35 +0300

    Main window development.
    -better file handling
    -show a tree of objects
    -show a window for editing

--- src/editor/mainwindow.cpp
+++ src/editor/mainwindow.cpp
@@ -1,24 +1,148 @@
 #include "mainwindow.hpp"
 
+#include "objectfactory.hpp"
+
 #include <QtGui/QApplication>
 #include <QFileDialog>
+#include <QMdiSubWindow>
 #include <fstream>
 #include <iostream>
 
-MainWindow::MainWindow(QWidget *parent) :
-	QMainWindow(parent),
-	ui(new Ui::MainWindow)
-{
-	ui->setupUi(this);
+#include "editform.hpp"
 
-	ui->saveAction->setEnabled(false);
-	ui->saveAsAction->setEnabled(false);
-	ui->closeAction->setEnabled(false);
+MainWindow::MainWindow(QWidget *parent) :
+	QMainWindow(parent)
+{
+	setupUi(this);
+
+	setFileActionsEnabled(false);
+}
+
+void MainWindow::initView()
+{
+	objectTreeWidget->setColumnCount(1);
+
+	vehicleItem = new QTreeWidgetItem((QTreeWidgetItem*)0, QStringList(QString("Autot")));
+
+	objectTreeWidget->insertTopLevelItem(0, vehicleItem);
+
+	newVehicleItem = new QTreeWidgetItem(vehicleItem, QStringList(QString("[Uusi auto]")));
+
+	newVehicleItem->setIcon(0, QIcon(QPixmap("data/editor/icons/newvehicle.png")));
+
+	for(Game::Vehicle* vehicle : state->getShopVehicles())
+	{
+		addVehicleItem(vehicle);
+	}
+
+	setFileActionsEnabled(true);
+}
+
+void MainWindow::newFile()
+{
+	closeFile();
+
+	state.reset(new Game::State(factory));
+
+	initView();
+}
+
+void MainWindow::openFile(const std::string& fileName)
+{
+	closeFile();
+
+	Json::Value stateJson;
+
+	std::ifstream(fileName) >> stateJson;
+
+	state.reset(new Game::State(stateJson, factory));
+
+	initView();
+}
+
+void MainWindow::saveFile()
+{
+	Json::Value stateJson;
+
+	state->save(stateJson);
+
+	std::ofstream(fileName) << stateJson;
+}
+
+void MainWindow::closeFile()
+{
+	state.reset();
+
+	objectTreeWidget->clear();
+
+	setFileActionsEnabled(false);
+}
+
+void MainWindow::setFileActionsEnabled(bool enabled)
+{
+	saveAction->setEnabled(enabled);
+	saveAsAction->setEnabled(enabled);
+	closeAction->setEnabled(enabled);
+	listContainer->setEnabled(enabled);
+}
+
+void MainWindow::openEditor(Game::Object* object)
+{
+	EditForm* child = new EditForm(object, this);
+
+	Qt::WindowFlags flags = Qt::CustomizeWindowHint;
+	flags |= Qt::WindowSystemMenuHint;
+	flags |= Qt::WindowTitleHint;
+	flags |= Qt::WindowCloseButtonHint;
+
+	QMdiSubWindow* subWindow = mdiArea->addSubWindow(child, flags);
+
+	subWindow->setAttribute(Qt::WA_DeleteOnClose);
+	child->setAttribute(Qt::WA_DeleteOnClose);
+
+	connect(child, SIGNAL(destroyed()), subWindow, SLOT(close()));
+	connect(child, SIGNAL(edited(Game::Object*)), this, SLOT(objectEdited(Game::Object*)));
+
+	child->show();
+}
+
+QTreeWidgetItem* MainWindow::findItemForObject(Game::Object* object)
+{
+	for(auto pair : itemObjects)
+	{
+		if(pair.second == object)
+			return pair.first;
+	}
+
+	return nullptr;
+}
+
+Game::Object* MainWindow::findObjectForItem(QTreeWidgetItem* item)
+{
+	if(itemObjects.find(item) != itemObjects.end())
+		return itemObjects[item];
+
+	return nullptr;
+}
+
+void MainWindow::addVehicleItem(Game::Vehicle* vehicle)
+{
+	QStringList texts(QString(vehicle->getName().c_str()));
+
+	QTreeWidgetItem* newItem = new QTreeWidgetItem((QTreeWidgetItem*)0, texts);
+
+	newItem->setIcon(0, QIcon(QPixmap("data/editor/icons/vehicle.png")));
+
+	int index = vehicleItem->indexOfChild(newVehicleItem);
+
+	vehicleItem->insertChild(index, newItem);
+
+	itemObjects[newItem] = vehicle;
 }
 
 void MainWindow::on_newAction_triggered()
 {
-
+	newFile();
 }
 
 void MainWindow::on_openAction_triggered()
@@ -28,27 +152,7 @@ void MainWindow::on_openAction_triggered()
 	if(fileName == "")
 		return;
 
-	Json::Value stateJson;
-
-	std::ifstream(fileName) >> stateJson;
-
-	Game::ObjectFactory factory;
-
-	state.reset(new Game::State(stateJson, factory));
-
-	vehicleModel.reset(new VehicleTableModel(state->getShopVehicles()));
-
-	TableView* vehicleView = ui->vehicleView;
-	vehicleView->setModel(vehicleModel.get());
-	vehicleView->hideAllColumns();
-	vehicleView->showColumn(vehicleModel->name.getIndex());
-	vehicleView->horizontalHeader()->setResizeMode(vehicleModel->name.getIndex(), QHeaderView::Stretch);
-
-
-
-	ui->saveAction->setEnabled(true);
-	ui->saveAsAction->setEnabled(true);
-	ui->closeAction->setEnabled(true);
+	openFile(fileName);
 }
 
 void MainWindow::on_saveAction_triggered()
@@ -59,11 +163,7 @@ void MainWindow::on_saveAction_triggered()
 		return;
 	}
 
-	Json::Value stateJson;
-
-	state->save(stateJson);
-
-	std::ofstream(fileName) << stateJson;
+	saveFile();
 }
 
 void MainWindow::on_saveAsAction_triggered()
@@ -75,27 +175,51 @@ void MainWindow::on_saveAsAction_triggered()
 
 	this->fileName = fileName;
 
-	Json::Value stateJson;
-
-	state->save(stateJson);
-
-	std::ofstream(fileName) << stateJson;
+	saveFile();
 }
 
 void MainWindow::on_closeAction_triggered()
 {
-	vehicleModel.reset(0);
-	ui->vehicleView->setModel(vehicleModel.get());
-
-
-	state.reset(0);
-
-	ui->saveAction->setEnabled(false);
-	ui->saveAsAction->setEnabled(false);
-	ui->closeAction->setEnabled(false);
+	closeFile();
 }
 
 void MainWindow::on_quitAction_triggered()
 {
 	QApplication::exit(0);
+}
+
+void MainWindow::on_objectTreeWidget_itemDoubleClicked(QTreeWidgetItem* selectedItem, int column)
+{
+	(void)column;
+
+	if(selectedItem == newVehicleItem)
+	{
+		Game::Object* object = Editor::ObjectFactory().createObject("Vehicle");
+		Game::Vehicle* vehicle = dynamic_cast<Game::Vehicle*>(object);
+
+		if(vehicle)
+		{
+			state->addShopVehicle(vehicle);
+
+			addVehicleItem(vehicle);
+
+			openEditor(vehicle);
+		}
+	}
+	else if(Game::Object* object = findObjectForItem(selectedItem))
+	{
+		openEditor(object);
+	}
+}
+
+void MainWindow::objectEdited(Game::Object* object)
+{
+	if(Game::Vehicle* vehicle = dynamic_cast<Game::Vehicle*>(object))
+	{
+		for(int i = 0; i < vehicleItem->childCount(); ++i)
+		{
+			if(QTreeWidgetItem* item = findItemForObject(object))
+				item->setText(0, QString(vehicle->getName().c_str()));
+		}
+	}
 }
--- src/editor/mainwindow.hpp
+++ src/editor/mainwindow.hpp
@@ -2,16 +2,16 @@
 #define EDITOR_MAINWINDOW_H
 
 #include <memory>
+#include <map>
 
 #include <QMainWindow>
 
 #include "ui_mainwindow.h"
 
-#include "models/vehicletablemodel.hpp"
 #include "game/state.hpp"
 #include "game/objectfactory.hpp"
 
-class MainWindow : public QMainWindow
+class MainWindow : public QMainWindow, private Ui::MainWindow
 {
 	Q_OBJECT
 
@@ -19,11 +19,29 @@ class MainWindow : public QMainWindow
 		explicit MainWindow(QWidget *parent = 0);
 
 	private:
-		std::unique_ptr<Ui::MainWindow> ui;
+		void newFile();
+		void openFile(const std::string& fileName);
+		void saveFile();
+		void closeFile();
+		void setFileActionsEnabled(bool enabled);
+		void openEditor(Game::Object* object);
+
+		void initView();
+		QTreeWidgetItem* findItemForObject(Game::Object* object);
+		Game::Object* findObjectForItem(QTreeWidgetItem* item);
+
+		void addVehicleItem(Game::Vehicle* vehicle);
+
+		Game::ObjectFactory factory;
+
 		std::unique_ptr<Game::State> state;
-		std::unique_ptr<VehicleTableModel> vehicleModel;
 
 		std::string fileName;
+
+		QTreeWidgetItem* vehicleItem;
+		QTreeWidgetItem* newVehicleItem;
+
+		std::map<QTreeWidgetItem*, Game::Object*> itemObjects;
 
 	private slots:
 		void on_newAction_triggered();
@@ -32,6 +50,10 @@ class MainWindow : public QMainWindow
 		void on_saveAsAction_triggered();
 		void on_closeAction_triggered();
 		void on_quitAction_triggered();
+
+		void on_objectTreeWidget_itemDoubleClicked(QTreeWidgetItem* item, int column);
+
+		void objectEdited(Game::Object* object);
 
 };
 
